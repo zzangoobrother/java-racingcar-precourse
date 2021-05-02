107456cc0e286523154ea563b6faf000509ecbd4
zzanguchoisk <chltjsrkd@hanmail.net>
2021-05-03 01:15:50 +0900

    test(Car) : 자동차 이름이 1이상, 5이하여야 한다. 자동차 이름 공백 test는 중복테스트로 삭제하였습니다.

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -2,13 +2,22 @@ package domain;
 
 public class Car {
 
+    private static final int CAR_NAME_LENGTH_MIN = 1;
+    private static final int CAR_NAME_LENGTH_MAX = 9;
+
     private final String name;
     private int forwardNumber;
 
     public Car(String name) {
-
+        validateLength(name);
         this.name = name;
         forwardNumber = 0;
+    }
+
+    private void validateLength(String name) {
+        if ((name.length() < CAR_NAME_LENGTH_MIN) || (name.length() > CAR_NAME_LENGTH_MIN)) {
+           throw new IllegalArgumentException();
+        }
     }
 
     public String getName() {
--- src/test/java/domain/CarTest.java
+++ src/test/java/domain/CarTest.java
@@ -2,13 +2,16 @@ package domain;
 
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class CarTest {
 
     @Test
-    public void 자동차_이름_공백() {
-        Car car = new Car("");
-        assertThat(car.getName().isEmpty()).isTrue();
+    public void 자동차_이름_길이_1이상_5이하() {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> {
+                    new Car("abcdef");
+                    new Car("");
+                });
     }
 }
