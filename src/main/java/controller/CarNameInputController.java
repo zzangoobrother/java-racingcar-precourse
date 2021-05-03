178a2eab7a43f7f769a3098385da433cc3a088d4
zzanguchoisk <chltjsrkd@hanmail.net>
2021-05-03 22:43:42 +0900

    feat(CarNameInputController) : 자동차 객체 To Cars List 생성하기 추가

--- src/main/java/controller/CarNameInputController.java
+++ src/main/java/controller/CarNameInputController.java
@@ -1,8 +1,27 @@
 package controller;
 
+import domain.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class CarNameInputController {
 
     public String[] divideName(String carName) {
         return carName.split(",");
     }
+
+    public List<Car> carsCreate(String carName) {
+        return getCars(divideName(carName));
+    }
+
+    public List<Car> getCars(String[] carNames) {
+        List<Car> cars = new ArrayList<>();
+
+        for (String name : carNames) {
+            cars.add(new Car(name));
+        }
+        return cars;
+    }
+
 }
