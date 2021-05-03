cc3bf31e6a9aef89c415ffd01a6ac00e4e2aaaa9
zzanguchoisk <chltjsrkd@hanmail.net>
2021-05-03 22:42:49 +0900

    test(CarNameInputControllerTest) : 자동차 객체 To Cars List 생성

--- src/test/java/controller/CarNameInputControllerTest.java
+++ src/test/java/controller/CarNameInputControllerTest.java
@@ -1,6 +1,9 @@
 package controller;
 
+import domain.Car;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -17,6 +20,19 @@ public class CarNameInputControllerTest {
         assertThat(divideName[0]).isEqualTo("pobi");
         assertThat(divideName[1]).isEqualTo("crong");
         assertThat(divideName[2]).isEqualTo("honux");
+    }
+
+    @Test
+    public void 자동차_객체_To_Cars() {
+        String carName = "pobi,crong,honux";
+
+        CarNameInputController carNameInputController = new CarNameInputController();
+
+        List<Car> cars = carNameInputController.carsCreate(carName);
+
+        assertThat(cars.get(0).getName()).isEqualTo("pobi");
+        assertThat(cars.get(1).getName()).isEqualTo("crong");
+        assertThat(cars.get(2).getName()).isEqualTo("honux");
     }
 
 }
